00928edc4774e2426b07eabac65ce460c9a61bb8
LionZXY <nikita@kulikof.ru>
2021-07-28 20:25:28 +0300

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i "s/SECRET_WORLD_SEED/$SECRET_WORLD_SEED/g" ./settings/world.yml
+sed -i "s/SECRET_WORLD_SEED/$SECRET_WORLD_SEED/g" ./settings/worlds.yml
 
 java -Xms${MC_RAM} -Xmx${MC_RAM} -XX:+UseG1GC -XX:+UseStringDeduplication -jar ultramine_core-1.7.10-server-0.1.5.jar nogui
