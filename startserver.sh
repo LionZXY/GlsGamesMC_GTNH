fd7731837d1128f9c52a18be5843bec76f5c7c43
LionZXY <nikita@kulikof.ru>
2021-07-28 21:08:25 +0300

    Remove fml confirm

--- startserver.sh
+++ startserver.sh
@@ -2,4 +2,4 @@
 
 sed -i "s/SECRET_WORLD_SEED/$SECRET_WORLD_SEED/g" ./settings/worlds.yml
 
-java -Xms${MC_RAM} -Xmx${MC_RAM} -XX:+UseG1GC -XX:+UseStringDeduplication -Dfml.queryResult=confirm -jar ultramine_core-1.7.10-server-0.1.5.jar nogui
+java -Xms${MC_RAM} -Xmx${MC_RAM} -XX:+UseG1GC -XX:+UseStringDeduplication ultramine_core-1.7.10-server-0.1.5.jar nogui
