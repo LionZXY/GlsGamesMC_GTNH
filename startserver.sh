c5ad7431106a038de54dba3b2f7df9b9faf04f9b
LionZXY <nikita@kulikof.ru>
2021-07-28 20:04:45 +0300

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -2,4 +2,4 @@
 
 sed -i "s/SECRET_WORLD_SEED/$SECRET_WORLD_SEED" ./settings/world.yml
 
-java -Xms${MC_RAM} -Xmx${MC_RAM} -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:InitiatingHeapOccupancyPercent=20 -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
+java -Xms${MC_RAM} -Xmx${MC_RAM} -XX:+UseG1GC -XX:+UseStringDeduplication -jar ultramine_core-1.7.10-server-0.1.5.jar nogui
